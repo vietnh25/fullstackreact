ad66bd25d53ae1516bae6cccb802d83417bd8ad5
howtolove488 <viethoang.nghiem@gmail.com>
2020-08-04 11:07:41 +0700

    pg with reactjs

---
+++ backend/product_manager_api/routes/index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'sanpham',
+  password: '123456',
+  port: 5432,
+})
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+});
+// api get data from postgreSql
+router.get('/getdata01', function(req, res, next) {   
+  
+  pool.query('SELECT * FROM public.product_info',(error,response) => {
+    if(error){
+      console.log(error);
+    }else {      
+      res.send(response.rows);
+    }    
+   // pool.end(); 
+  })
+   
+});
+
+router.get('/add', function(req, res, next) { 
+  res.render('add',{});
+});
+
+router.post('/add', function(req, res, next) { 
+
+  var product_name = req.body.product_name,
+  product_price = req.body.product_price,
+  image = req.body.image  ; 
+
+  pool.query("INSERT INTO product_info (product_name,product_price,image) values ($1,$2,$3)",[product_name,product_price,image],(err,response)=>{
+    if(err) {
+      res.send(err);
+      res.send(0);
+    }
+    else {
+      res.send(1);
+    }
+  })
+ 
+  
+});
+
+module.exports = router;
---
+++ frontend/product_manager_frontend/src/components/HeadTitle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HeadTitle extends Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1 className="display-3">Danh sách hiển thị sản phẩm</h1>
+                <p className="lead">Sử dụng reactjs lấy dữ liệu nodejs</p>
+                <hr className="my-2" />
+            </div>
+        );
+    }
+}
+
+export default HeadTitle;
